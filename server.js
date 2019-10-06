265be5b81e4649d795d7cc837818156f60b2548d
Namit Pathak <NamitPathak@users.noreply.github.com>
2019-10-07 01:30:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Java Home App - version-4!!</h1> \n');
+  res.send('Hello World');
 });
 
 app.listen(PORT, HOST);

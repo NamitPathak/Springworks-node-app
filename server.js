d2f21104f76a8c7baa6ee126556df308ea7c0570
Namit Pathak <NamitPathak@users.noreply.github.com>
2019-10-07 18:15:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Springworks !!');
+  res.send('Hello Springworks Hope You will like my work and hire me !!');
 });
 
 app.listen(PORT, HOST);
